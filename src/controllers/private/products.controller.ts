f9052540054e9b8bbf953a9b5a679d21f5e63144
จิรทีปต์ ลอยมา <jirateep.loyma02@gmail.com>
2023-05-30 09:26:49 +0700

    fix - products controller

--- src/controllers/private/products.controller.ts
+++ src/controllers/private/products.controller.ts
@@ -1,13 +1,28 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 import * as productsService from '../../services/products.service';
 import { IProduct } from '../../dtos/product.dto';
+import { prisma } from '../../plugins/prisma';
 
 async function productsPrivateRoutes(fastify: FastifyInstance, opts: any) {
   fastify.post(
     '/products',
-    // { preValidation: [fastify.authenticate] },
+    { preValidation: [fastify.authenticate] },
     async (request: FastifyRequest<{ Body: IProduct; Headers: { authorization: string } }>, reply: FastifyReply) => {
       try {
+        const auth = request.headers.authorization;
+        const token = auth.split(' ')[1];
+        const decodedToken: any = fastify.jwt.decode(token);
+        const { userId } = decodedToken;
+
+        const adminCheck = await prisma.user.findUnique({
+          where: {
+            id: Number(userId)
+          }
+        });
+
+        if (adminCheck?.isAdmin === false) {
+          return reply.status(401).send({ message: 'Unauthorized' });
+        }
         const products = await productsService.createProduct(request.body);
         reply.status(201).send(products);
       } catch (err) {
@@ -21,7 +36,7 @@ async function productsPrivateRoutes(fastify: FastifyInstance, opts: any) {
 
   fastify.patch(
     '/products/:id',
-    // { preValidation: [fastify.authenticate] },
+    { preValidation: [fastify.authenticate] },
     async (
       request: FastifyRequest<{ Body: IProduct; Params: { id: string }; Headers: { authorization: string } }>,
       reply: FastifyReply
@@ -37,7 +52,7 @@ async function productsPrivateRoutes(fastify: FastifyInstance, opts: any) {
 
   fastify.delete(
     '/products/:id',
-    // { preValidation: [fastify.authenticate] },
+    { preValidation: [fastify.authenticate] },
     async (request: FastifyRequest<{ Params: { id: string }; Headers: { authorization: string } }>, reply: FastifyReply) => {
       try {
         const products = await productsService.deleteProduct(Number(request.params.id));
